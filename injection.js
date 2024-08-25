1c9e89c2ceffbcbbaaa1521bbba6cb9ba23bd554
omen <132889586+lugloose@users.noreply.github.com>
2024-08-25 17:37:20 +0100

    Update injection.js

--- injection.js
+++ injection.js
@@ -358,7 +358,7 @@ function modifyCode(text) {
 		if (_ == "GrandDad") {
 			const $ = skins[_];
 			return new Promise((et, tt) => {
-				textureManager.loader.load("https://raw.githubusercontent.com/7GrandDadPGN/VapeForMiniblox/main/assets/skin.png", rt => {
+				textureManager.loader.load("https://raw.githubusercontent.com/lugloose/VapeForMiniblox/main/assets/skin.png", rt => {
 					const nt = {
 						atlas: rt,
 						id: _,
